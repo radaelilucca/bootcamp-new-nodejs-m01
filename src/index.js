2d71807d23a75e02c06da87e935a4121fcb3aaff
Lucca Radaeli <radaelidev@gmail.com>
2020-06-29 18:55:46 -0300

    Test Routes for all methods and params

--- src/index.js
+++ src/index.js
@@ -2,10 +2,65 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.json({
-    message: "Hello World"
-  })
+app.use(express.json())
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar infos do back-end;
+ * POST: Criar uma info no back-end;
+ * PUT/PATCH: Alterar uma info no back-end;
+ * DELETE: Deletar uma info do back-end;/
+ *
+ */
+
+ /**
+  * Tipos de parametros:
+  * 
+  * Query Params: Principalmente pra filtros e paginação;
+  * Route Params: Identificar recursos na hora de deletar ou atualizar;
+  * Request Body: Conteúdo pra criar ou editar um recurso - JSON;
+  */
+
+app.get('/projects', (req, res) => {
+  const {title, owner} = req.query;
+
+  // console.table({title, owner})
+
+  return res.json([
+    'Projeto 01',
+    'Projeto 02'
+  ])
+})
+
+app.post('/projects', (req, res) => {
+  const {title, owner} = req.body;
+  console.table({title, owner})
+  return res.json([
+    'Projeto 01',
+    'Projeto 02',
+    'Projeto 03'
+  ])
+})
+
+app.put('/projects/:id', (req, res) => {
+  const {id} = req.params;
+  const {title, owner} = req.body;
+  console.table({title, owner})
+ 
+  return res.json([
+    'Projeto 01',
+    'Projeto 02',
+    'Projeto 03'
+  ])
+})
+
+app.delete('/projects/:id', (req, res) => {
+  return res.json([
+    'Projeto 01',
+    'Projeto 02',
+    'Projeto 03'
+  ])
 })
 
 app.listen(3333, () => {
